ec7dd5456956ab9257c2e12872cdb3bf45e7a379
lwrb172 <lwrbielak@gmail.com>
2024-05-04 18:38:28 +0200

    lab4z1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(Person.fromCsvLine("Ewa Kowalska,03.11.1901,05.03.1990,,"));
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Person {
+    private final String name;
+    private final LocalDate birthDate;
+    private final LocalDate deathDate;
+
+    public Person(String name, LocalDate birthDate, LocalDate deathDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.deathDate = deathDate;
+    }
+
+    public static Person fromCsvLine(String line) {
+        String[] parts = line.split(",", -1);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate birthDate = LocalDate.parse(parts[1], formatter);
+        LocalDate deathDate = parts[2].isEmpty() ? null : LocalDate.parse(parts[2], formatter);
+
+        return new Person(parts[0], birthDate, deathDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                ", deathDate=" + deathDate +
+                '}';
+    }
+}
